ce6e5bd256f24ad790152065b289c246f516c1f4
link-short <dora.family228@gmail.com>
2025-11-09 11:54:19 +0500

    Create Loading.svelte

---
+++ src/assets/Loading.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import { IconLoader2 } from "@tabler/icons-svelte";
+
+    export let color = "var(--primary)";
+    export let size = "3em";
+</script>
+
+<div class="loading" style="color: {color}">
+    <IconLoader2 size={size} />
+</div>
+
+<style>
+    .loading {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+    :global(.loading svg) {
+        animation: spin 1s linear infinite;
+    }
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+</style>
